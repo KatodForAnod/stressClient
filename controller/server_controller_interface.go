58f105937d1a156f32aed275caa51488afca6930
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-07-08 14:21:29 +0300

    controller interface added

--- controller/server_controller_interface.go
+++ controller/server_controller_interface.go
@@ -1,1 +1,13 @@
 package controller
+
+import "stressClient/iot"
+
+type ServerController interface {
+	AddIot(iotDevice iot.IoT) error
+	RemoveIot(id int) error
+	GetInformationIotDevFunc1(id int) (string, error)
+	GetInformationIotDevFunc2(id int) (string, error)
+	GetInformationIotDevFunc3(id int) (string, error)
+	GetInformationIotDevFunc4(id int) (string, error)
+	GetInformationIotDevFunc5(id int) (string, error)
+}
