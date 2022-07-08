afaa3a5d6735b0deb7637879a196293b106ed9e9
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-07-08 15:41:40 +0300

    main sample

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	log "github.com/sirupsen/logrus"
+	"stressClient/config"
+	"stressClient/controller"
+	"stressClient/iot"
+	"stressClient/server"
+)
 
+func main() {
+	loadConfig, err := config.LoadConfig()
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	servcontroller := controller.Controller{}
+	servcontroller.Init()
+
+	for _, device := range loadConfig.IoTsDevices {
+		iotCommon := iot.CommonIotDevice{}
+		iotCommon.Init(device)
+
+		err = servcontroller.AddIot(&iotCommon)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
+
+	server := server.Server{}
+	server.StartServer(loadConfig, &servcontroller)
 }
